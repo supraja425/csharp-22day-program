3ecb2f2f23b2214e076b9905a944dbbd12c1574a
supraja425 <suprajayelagandula@gmail.com>
2026-06-12 02:49:48 -0700

    Ledger Validations

---
+++ Day08/Labs/EHRMvcAuditLedgerDem/EHRMvcAuditLedgerDem/Views/Transaction/Success.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EHRMvcAuditLedgerDem.Views.Transaction
+{
+    public class SuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Day08/Labs/EHRMvcAuditLedgerDem/EHRMvcAuditLedgerDem/Views/Transaction/Verify.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EHRMvcAuditLedgerDem.Views.Transaction
+{
+    public class VerifyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
